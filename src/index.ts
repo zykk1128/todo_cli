73a865a41f83c6284e7dcb75f2b6246662e3a1eb
kk_JLYao <307152298@qq.com>
2025-06-24 16:31:20 +0800

    feat: 初始化项目

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import {Command} from 'commander';
+import {version} from '../package.json';
+import {create} from './command/create';
+// import { update } from './command/update';
+const program = new Command('pokemon');
+program.version(version, '-v --version');
+
+program
+    .command('update')
+    .description('更新脚手架 todo-cli')
+    .action(async () => {
+        // await update();
+    });
+
+program.command('create').description('创建一个新项目').argument('[name]', '项目名称').action(async (dirName) => {
+    if (dirName) {
+        create(dirName);
+    } else {
+        console.log(`create ${dirName}`)
+    }
+    });
+
+program.parse();
